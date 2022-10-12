988fe69d530767ce6976676dbbad2058f929d83e
Karl Winterling <kwinterling@gmail.com>
2022-10-12 15:17:03 -0700

    Finished Deliverable 2

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -29,3 +29,18 @@ SELECT COUNT(emp_no) AS count, title
     FROM unique_titles
     GROUP BY title
     ORDER BY count DESC;
+
+
+-- Deliverable 2
+
+-- Get employees eligible for mentorship program
+SELECT DISTINCT ON (emp_no) employees.emp_no AS emp_no, first_name, last_name, birth_date,
+    dept_emp.from_date AS from_date, dept_emp.to_date AS to_date, titles.title AS title
+    INTO mentorship_eligibility
+    FROM employees INNER JOIN dept_emp
+        ON employees.emp_no = dept_emp.emp_no INNER JOIN titles
+            ON employees.emp_no = titles.emp_no
+    WHERE (dept_emp.to_date = '9999-01-01') AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+    ORDER BY emp_no;
+
+
